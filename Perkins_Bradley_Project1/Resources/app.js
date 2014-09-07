aa6f36b2656fdd64a171a63fd892c46b34e20821
Brad Perkins <bperkins99@fullsail.edu>
2014-09-06 21:56:14 -0400

    finished app

--- Perkins_Bradley_Project1/Resources/app.js
+++ Perkins_Bradley_Project1/Resources/app.js
@@ -1,8 +1,6 @@
 
 Ti.UI.setBackgroundColor("white");
 
-var comics = ["Amazing Spider-Man", "Batman", "Batman and Robin", "Redhood and the Outlaws", "The Walking Dead"];
-
 var mainWin = Ti.UI.createWindow({
 	title: "My Top 5 Favorite Comic",
 	backgroundColor: "#6897bb",
@@ -64,56 +62,7 @@ var nextText = Ti.UI.createLabel({
 	text: "Next"
 });
 
-var i = -1;
-
-var nextComics = function(){	
-	if(i ===comics.length -1){
-		i = -1;
-	};
-	i++;
-	
-		var comicList = Ti.UI.createLabel({
-		text: comics[i],
-		textAlign: "center",
-		backgroundColor: "white",
-		borderRadius: 2,
-		borderWidth: 1,
-		top: 20,
-		left: 20,
-		right: 20,
-		bottom: 100
-	});
-
-	
-	mainWin.add(comicList);
-		
-	};
-
-var prevComics = function(){
-	//view1.hide();	
-	if(i ===0){
-		i = comics.length;
-	};
-	i--;
-	
-		var comicList = Ti.UI.createLabel({
-		text: comics[i],
-		textAlign: "center",
-		backgroundColor: "white",
-		borderRadius: 2,
-		borderWidth: 1,
-		top: 20,
-		left: 20,
-		right: 20,
-		bottom: 100
-	});
-	
-	mainWin.add(comicList);
-		
-};
-
-nextButton.addEventListener("click", nextComics);
-prevButton.addEventListener("click", prevComics);
+var loadFile = require("app2");
 
 mainWin.add(view1, prevButton, nextButton);
 view1.add(title);
